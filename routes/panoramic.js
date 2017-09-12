05cd183d37251cba33d723b73ea6945b83e8018d
Andrew Eskenazi <drewesk@gmail.com>
2017-09-11 20:35:31 -0600

    Typo fix!

--- routes/panoramic.js
+++ routes/panoramic.js
@@ -6,63 +6,63 @@ const API_URL = '/panoramic';
 
 router.get(API_URL, async(ctx) => {
   try {
-    const Pan = await panoramic.getAll();
+    const PAN = await panoramic.getAll();
     ctx.body = {
       status: 'success',
-      data: Pan
+      data: PAN
     };
   } catch (err) {
     ctx.status = 404;
     ctx.body = {
       status: 'error',
-      message: err.message || 'Oops, Something we wrong...'
+      message: err.message || 'Oops, Something went wrong...'
     };
   }
 });
 
 router.get(`${API_URL}/:id`, async(ctx) => {
   try {
-    const Pan = await panoramic.get(ctx.params.id);
-    if (panoramic) {
+    const PAN = await panoramic.get(ctx.params.id);
+    if (PAN.length) {
       ctx.body = {
         status: 'success',
-        data: Pan
+        data: PAN
     };
   } else {
       ctx.status = 404;
       ctx.body = {
-        status: 'error',
+        status: 'error'
       }
-  }
+    }
   } catch (err) {
     ctx.status = 404;
-    ctx.body ={
+    ctx.body = {
       status: 'error',
-      message: err.message || 'Oops, Something we wrong...'
+      message: err.message || 'Oops, Something went wrong...'
     };
   }
 });
 
 router.post(`${API_URL}`, async (ctx) => {
   try {
-    const Pan = await panoramic.create(ctx.request.body);
-    if (panoramic) {
+    const PAN = await panoramic.create(ctx.request.body);
+    if (PAN.length) {
       ctx.status = 200;
       ctx.body = {
         status: 'success',
-        data: Pan
+        data: PAN
       };
     } else {
       ctx.status = 404;
       ctx.body = {
-        status: 'error',
+        status: 'error'
       };
     }
   } catch (err) {
     ctx.status = 404;
     ctx.body = {
       status: 'error',
-      message: err.message || 'Oops, something we wrong...'
+      message: err.message || 'Oops, something went wrong...'
     };
   }
 });
@@ -79,14 +79,14 @@ router.put(`${API_URL}/:id`, async (ctx) => {
     } else {
       ctx.status = 404;
       ctx.body = {
-        status: 'error',
+        status: 'error'
       };
     }
   } catch (err) {
     ctx.status = 404;
     ctx.body = {
       status: 'error',
-      message: err.message || 'Oops, something we wrong...'
+      message: err.message || 'Oops, something went wrong...'
     }
   }
 });
@@ -103,7 +103,7 @@ router.delete(`${API_URL}/:id`, async (ctx) => {
     } else {
       ctx.status = 404;
       ctx.body = {
-        status: 'error',
+        status: 'error'
       };
     }
   } catch (err) {
