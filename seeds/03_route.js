b9bb9fe62859e5f0f4c3fda6b12009de2783eec0
Andrew Eskenazi <drewesk@gmail.com>
2017-09-18 02:38:13 -0600

    Adding Dummy Data for the routes seed.

--- seeds/03_route.js
+++ seeds/03_route.js
@@ -1,21 +1,79 @@
 exports.seed = (knex) => {
   return knex.raw('TRUNCATE "Route" RESTART IDENTITY CASCADE;')
     .then(() => {
-      return knex('Route').insert([
-        {
+      return knex('Route').insert([{
           name: 'crack of dawn',
+          location: 'Cannon Beach, OR'
           gradeType: 'Hueco',
           grade: 'V4',
           climbType: 'Bouldering'
         },
         {
           name: 'under the cove',
+          location: 'Boulder-Creek, Boulder, Co'
           gradeType: 'YDS',
           grade: '5.11',
           climbType: 'Top-Rope'
         },
         {
-          name: 'falling rocks',
+          name: 'Falling Rocks',
+          location: 'Earth Treks, Golden, Co'
+          gradeType: 'YDS',
+          grade: '5.15b',
+          climbType: 'Free-Solo'
+        },
+        {
+          name: 'Sedoku\'s Brother',
+          location: 'The Spot, Boulder, Co',
+          gradeType: 'Hueco',
+          grade: 'V4',
+          climbType: 'Bouldering'
+        },
+        {
+          name: 'Crag Me Up',
+          location: 'Movements Climbing Gym, broadway st. denver, Co'
+          gradeType: 'YDS',
+          grade: '5.11',
+          climbType: 'Top-Rope'
+        },
+        {
+          name: 'Hang Loose',
+          location: 'RedRocks, Denver',
+          gradeType: 'YDS',
+          grade: '5.15b',
+          climbType: 'Free-Solo'
+        },
+        {
+          name: 'Twister',
+          location: 'Rock\'n and Jam\'n, Thornton, Co'
+          gradeType: 'YDS',
+          grade: '5.11',
+          climbType: 'Top-Rope'
+        },
+        {
+          name: 'Walk in the Park',
+          location: 'Rock\'n and Jam\'n, Centennial, Co'
+          gradeType: 'YDS',
+          grade: '5.15b',
+          climbType: 'Free-Solo'
+        },
+        {
+          name: 'Mighty Feast',
+          location: 'Bear Creek trail, Glendale, Co'
+          gradeType: 'Hueco',
+          grade: 'V4',
+          climbType: 'Bouldering'
+        },
+        {
+          name: 'Natures End',
+          location: 'RedRocks, Denver, Co',
+          gradeType: 'YDS',
+          grade: '5.11',
+          climbType: 'Top-Rope'
+        },
+        {
+          name: 'ElCapitan',
+          location: 'Yosemite National Park, California'
           gradeType: 'YDS',
           grade: '5.15b',
           climbType: 'Free-Solo'
